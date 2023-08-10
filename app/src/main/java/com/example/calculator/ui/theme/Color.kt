07ff66a212498fefee2cea5a1f52462dc1f6d001
K2E7 <k2e7@outlook.com>
2023-08-10 15:09:20 +0530

    Hello this is the first iteration of Team R37URN0's calculator application.

---
+++ app/src/main/java/com/example/calculator/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.calculator.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+
+val DarkMode_Primary = Color(0xff22252d)
+val DarkMode_Secondary = Color(0xff292d36)
+
+val LightMode_Primary = Color(0xffffffff)
+val LightMode_Secondary = Color(0xfff6f6f6)
+
+val DarkMode_Green = Color(0xff4dd9c5)
+val DarkMode_Red   = Color(0xffc7677a)
+
+val LightMode_Green = Color(0xff2d8786)
+val LightMode_Red   = Color(0xffa70821)
